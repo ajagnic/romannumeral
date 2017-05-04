40635083b27e9510851b469a68a8678679b83eb1
Adrian Agnic and Roger Tobias <adrian+roger@>
2017-05-04 10:17:08 -0700

    adds the business logic for the function that converts to the roman numeral.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,32 @@
 $(document).ready(function(){
   $("#roman").submit(function(event){
     event.preventDefault();
-    debugger;
     var userNumber = parseInt($("input#userInput").val());
 
-    if (isNaN(userInput)) {
+    if (isNaN(userNumber)) {
       alert("Please only input numbers.")
+    } else {
+      var result = getRomanNum(userNumber);
     }
 
-
+    $("#output").text(result);
   });
-
-  
-
 });
+
+//-----------------------------------BACKEND LOGIC
+var romanSymbol = [[1000, "M"], [500, "D"], [100, "C"], [50, "L"], [10, "X"], [5, "V"], [1, "I"]];
+var tempArray = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
+
+var getRomanNum = function(userNumber) {
+  debugger;
+  if (userNumber === 0) {
+    return '';
+  } else {
+    for (i = 0; i <= 7; i++) {
+      if (userNumber >= romanSymbol[i][0]) {
+        var result = romanSymbol[i][1] + getRomanNum(userNumber - romanSymbol[i][0]);
+        return result;
+      }
+    }
+  }
+};
